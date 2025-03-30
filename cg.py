1a405427cfc1fadc3e33e8d82816bb54c58c44a1
Chris-912 <osekochris612@gmail.com>
2025-03-30 18:41:22 +0300

    Update cg.py

--- cg.py
+++ cg.py
@@ -1,12 +1,12 @@
 #compound intrest calculator(time in years)
 principle = 0
-rate = 0
+rate = 100
 time = 0
 principle = float(input("Enter principle "))
 rate = float(input("Enter rate " ))
 time = float(input("Enter time "))
 
-if principle<=0 or rate <=0 and time <=0:
+if principle<=0 or rate >=0 or time <=0:
     print("invalid input")
 else:
     print("Amount")
